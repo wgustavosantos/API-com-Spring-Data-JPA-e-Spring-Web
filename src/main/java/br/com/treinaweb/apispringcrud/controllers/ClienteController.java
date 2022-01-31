2b57c6cc405751329a94fcbeee46c45e211fe45d
Wenderson Santos <guto15santos@gmail.com>
2022-01-30 21:59:35 -0300

    Rota de listagem de clientes

--- src/main/java/br/com/treinaweb/apispringcrud/controllers/ClienteController.java
+++ src/main/java/br/com/treinaweb/apispringcrud/controllers/ClienteController.java
@@ -1,9 +1,14 @@
 package br.com.treinaweb.apispringcrud.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.treinaweb.apispringcrud.entities.Cliente;
@@ -17,9 +22,16 @@ public class ClienteController {
 	private ClienteRepository clienteRepository;
 	
 	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
 	public Cliente cadastrar(@RequestBody Cliente cliente) {
 		
 		return clienteRepository.save(cliente);
+	}
+	
+	@GetMapping 
+	public List<Cliente> listar() {
+		
+		return clienteRepository.findAll();
 	}
 
 }
